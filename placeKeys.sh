585ef1e3cc16d107ee22e766dc8ddcdc23bb4a59
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-27 21:15:31 +0300

    key file in progress

--- placeKeys.sh
+++ placeKeys.sh
@@ -9,22 +9,30 @@ fi
 
 # Write keys
 echo "Enter SSLCertificateFile cert.pem (Ctrl+d to end)"
-read -r -d $'\04' key
+while IFS= read -r line; do
+    key+="$line"$'\n'
+done
 echo -e "$key" > $directory/cert.pem
 unset key
 
 echo -e "\nEnter SSLCertificateKeyFile privkey.pem (Ctrl+d to end)"
-read -r -d $'\04' key
+while IFS= read -r line; do
+    key+="$line"$'\n'
+done
 echo -e "$key" > $directory/privkey.pem
 unset key
 
 echo -e "\nEnter SSLCertificateChainFile chain.pem (Ctrl+d to end)"
-read -r -d $'\04' key
+while IFS= read -r line; do
+    key+="$line"$'\n'
+done
 echo -e "$key" > $directory/chain.pem
 unset key
 
 echo -e "\nEnter SSLCACertificateFile fullchain.pem (Ctrl+d to end)"
-read -r -d $'\04' key
+while IFS= read -r line; do
+    key+="$line"$'\n'
+done
 echo -e "$key" > $directory/fullchain.pem
 
 echo -e "\nCertificate files were created at: $directory"
