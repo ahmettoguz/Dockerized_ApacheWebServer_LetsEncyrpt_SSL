f7d9f4dbb4d6f20f2660f4f9f03ab0c1ef4d9e80
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-27 21:24:17 +0300

    bash sh files in progress.

--- placeKeys.sh
+++ placeKeys.sh
@@ -9,31 +9,22 @@ fi
 
 # Write keys
 echo "Enter SSLCertificateFile cert.pem (Ctrl+d to end)"
-while IFS= read -r line || [[ -n "$line" ]]; do
-    key+="$line"$'\n'
-done
-
-printf "%s" "$key" > "$directory/cert.pem"
+read -d $'\04' key
+echo -e "$key" > $directory/cert.pem
 unset key
 
 echo -e "\nEnter SSLCertificateKeyFile privkey.pem (Ctrl+d to end)"
-while IFS= read -r line || [[ -n "$line" ]]; do
-    key+="$line"$'\n'
-done
-printf "%s" "$key" > "$directory/privkey.pem"
+read -d $'\04' key
+echo -e "$key" > $directory/privkey.pem
 unset key
 
 echo -e "\nEnter SSLCertificateChainFile chain.pem (Ctrl+d to end)"
-while IFS= read -r line || [[ -n "$line" ]]; do
-    key+="$line"$'\n'
-done
-printf "%s" "$key" > "$directory/chain.pem"
+read -d $'\04' key
+echo -e "$key" > $directory/chain.pem
 unset key
 
 echo -e "\nEnter SSLCACertificateFile fullchain.pem (Ctrl+d to end)"
-while IFS= read -r line || [[ -n "$line" ]]; do
-    key+="$line"$'\n'
-done
-printf "%s" "$key" > "$directory/fullchain.pem"
+read -d $'\04' key
+echo -e "$key" > $directory/fullchain.pem
 
 echo -e "\nCertificate files were created at: $directory"
