9ef9db84a179b011cc0b8efd6a91f106dec716b2
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-27 20:57:44 +0300

    keys in progress

---
+++ placeKeys.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+directory="./src/certificate"
+
+# Create the directory if it doesn't exist
+if [ ! -d "$directory" ]; then
+    mkdir -p "$directory"
+fi
+
+# Write keys
+echo "Enter SSLCertificateFile cert.pem (Ctrl+d to end)"
+read -d $'\04' key
+echo -e "$key" > $directory/cert.pem
+unset key
+
+echo -e "\nEnter SSLCertificateKeyFile privkey.pem (Ctrl+d to end)"
+read -d $'\04' key
+echo -e "$key" > $directory/privkey.pem
+unset key
+
+echo -e "\nEnter SSLCertificateChainFile chain.pem (Ctrl+d to end)"
+read -d $'\04' key
+echo -e "$key" > $directory/chain.pem
+unset key
+
+echo -e "\nEnter SSLCACertificateFile fullchain.pem (Ctrl+d to end)"
+read -d $'\04' key
+echo -e "$key" > $directory/fullchain.pem
+
+echo -e "\nCertificate files were created at: $directory"
